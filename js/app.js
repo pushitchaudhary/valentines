3f73a7c89a1952852450eea4854a2f5ed5e6de64
Pushit Chaudhary <pushitchaudhary@Pushits-MacBook-Air.local>
2026-02-10 13:19:40 +0545

    fix

--- js/app.js
+++ js/app.js
@@ -58,7 +58,7 @@ yes.addEventListener("click", () => {
   vid.style.display = "block";
   gif.src = "https://pushitchaudhary.github.io/valentines/resources/idc.gif";// gif credit: https://tenor.com/view/peach-cat-kiss-animated-love-mwah-gif-25743978
   text.innerHTML = "Knew it babe 😘";
-  yes.innerHTML = '<a href="https://www.instagram.com/loop.codes/">Message me</a>';// inside the " " put your social media profile link
+  yes.innerHTML = '<a href="https://wa.me/+9779811288084">Message me</a>';// inside the " " put your social media profile link
   yes.style.height = "90%";
   yes.style.width = "96%";
   no.style.display = "none";
